d02240280443d50ecc3631bd9812186776e19a7c
wangshuang <wangshuang@em3.ai>
2021-11-23 11:04:47 +0800

    feat: user_srv集成nacos

--- user_srv/settings/settings.py
+++ user_srv/settings/settings.py
@@ -1,22 +1,60 @@
 from playhouse.pool import PooledMySQLDatabase
 from playhouse.shortcuts import ReconnectMixin
 
+from loguru import logger
+
+import nacos
+
 
 class ReconnectMysqlDatabase(ReconnectMixin, PooledMySQLDatabase):
     pass
 
 
-MYSQL_DB = "mxshop_user_srv"
-MYSQL_HOST = "192.168.1.103"
-MYSQL_PORT = 3306
-MYSQL_USER = "ws"
-MYSQL_PASSWORD = "123456"
+NACOS = {
+    "Host": "192.168.1.103",
+    "Port": 8848,
+    "NameSpace": "cc002bf5-cf66-4e9f-bda9-d56d74cce2f3",
+    "User": "nacos",
+    "Password": "nacos",
+    "DataId": "user_srv.json",
+    "Group": "dev",
+}
 
-CONSUL_HOST = "192.168.1.103"
-CONSUL_PORT = 8500
+client = nacos.NacosClient("{}:{}".format(NACOS["Host"], NACOS["Port"]), namespace=NACOS["NameSpace"],
+                           username=NACOS["User"], password=NACOS["Password"])
 
-SERVICE_NAME = "user_srv"
-SERVICE_TAGS = ["python"]
+import json
 
-DB = ReconnectMysqlDatabase(MYSQL_DB, host=MYSQL_HOST, port=MYSQL_PORT,
-                            user=MYSQL_USER, password=MYSQL_PASSWORD)
+json_data = json.loads(client.get_config(NACOS["DataId"], NACOS["Group"]))
+logger.info(json_data)
+
+
+# 监控配置文件的变化
+def test_cb(args):
+    logger.info("配置文件产生变化")
+    json_data = json.loads(args.get('raw_content'))
+    logger.info(json_data)
+    # 重新赋值全局变量
+    global CONSUL_HOST, CONSUL_PORT, SERVICE_NAME, SERVICE_TAGS, DB
+    CONSUL_HOST = json_data["consul"]["host"]
+    CONSUL_PORT = json_data["consul"]["port"]
+    SERVICE_NAME = json_data["name"]
+    SERVICE_TAGS = json_data["tags"]
+    DB = ReconnectMysqlDatabase(json_data["mysql"]["db"], host=json_data["mysql"]["host"],
+                                port=json_data["mysql"]["port"],
+                                user=json_data["mysql"]["user"], password=json_data["mysql"]["password"])
+
+
+client.add_config_watcher(NACOS["DataId"], NACOS["Group"], test_cb)
+import time
+time.sleep(3000)
+
+CONSUL_HOST = json_data["consul"]["host"]
+CONSUL_PORT = json_data["consul"]["port"]
+
+SERVICE_NAME = json_data["name"]
+SERVICE_TAGS = json_data["tags"]
+
+DB = ReconnectMysqlDatabase(json_data["mysql"]["db"], host=json_data["mysql"]["host"], port=json_data["mysql"]["port"],
+                            user=json_data["mysql"]["user"], password=json_data["mysql"]["password"])
+
